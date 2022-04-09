d0fdb8b602c76027990250044801848193adf995
VodzichY <yuri.vodich@gmail.com>
2022-04-10 01:57:47 +0300

    changed package thml to express for routing

--- src/dbMongo.js
+++ src/dbMongo.js
@@ -1,6 +1,5 @@
 const  dbMongo = require("../appMongoDb");
 const { ObjectId } = require("mongodb");
-const operationCommon = require("./operationCommon");
 
 async function getPersons(params) {
   const dataBase = dbMongo.db("dbTest");
@@ -14,41 +13,34 @@ async function getPersons(params) {
 }
 
 async function postPerson(params) {
-  const dataParsed = operationCommon.parseData(params.body);
-
   const dataBase = dbMongo.db("dbTest");
-  const persons = dataBase.collection("persons");
+  const collectionPersons = dataBase.collection("persons");
 
-  const cursor = await persons.insertOne(dataParsed);
-  const personStringifyed = operationCommon.unParseData(dataParsed);
-  return personStringifyed;
+  const result = await collectionPersons.insertOne(params.body);
+  return result;
 }
 
 async function getPersonById(params) {
   const dataBase = dbMongo.db("dbTest");
-  const persons = dataBase.collection("persons");
+  const personsCollection = dataBase.collection("persons");
 
   const query = { _id: ObjectId(params.query.id) };
   const options = {};
 
-  const cursor = await persons.findOne(query, options);
-  const personStringifyed = operationCommon.unParseData(cursor);
-  return personStringifyed;
+  const person = await personsCollection.findOne(query, options);
+  return person;
 }
 
 async function putPersonById(params) {
-  const dataParsed = operationCommon.parseData(params.body);
-
   const dataBase = dbMongo.db("dbTest");
-  const persons = dataBase.collection("persons");
+  const personsCollection= dataBase.collection("persons");
 
-  const query = { _id: ObjectId(dataParsed.id) };
+  const query = { _id: ObjectId(params.query.id) };
   const options = { upsert: false };
-  const updateDoc = { $set: dataParsed };
+  const updateDoc = { $set: params.body };
 
-  const result = await persons.updateOne(query, updateDoc, options);
-  const personStringifyed = operationCommon.unParseData(result);
-  return personStringifyed;
+  const result = await personsCollection.updateOne(query, updateDoc, options);
+  return result;
 
 }
 
--- src/personRouter.js
+++ src/personRouter.js
@@ -6,4 +6,19 @@ router.route("/").get(async (req, res, next) => {
   res.json(users);
 });
 
+router.route("/:id").get(async (req, res, next) => {
+  const user = await servicePerson.getPersonById(req);
+  res.json(user);
+});
+
+router.route("/").post(async (req, res, next) => {
+  const user = await servicePerson.postPerson(req);
+  res.json(user);
+});
+
+router.route("/:id").put(async (req, res, next) => {
+  const user = await servicePerson.putPersonById(req);
+  res.json(user);
+});
+
 module.exports = router;
