7f42909e64a8e70c5b6afffe147cf42b3014a2d0
VodzichY <yuri.vodich@gmail.com>
2022-04-13 00:36:07 +0300

    changed package thml to express for routing

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const { PORT } = require("./src/config");
-const app = require("./app");
+const appExpress = require("./appExpress");
 const dbMongo = require("./appMongoDb");
 
 dbMongo.connect();
@@ -7,7 +7,7 @@ dbMongo.connect();
 dbMongo.on('error', console.error.bind(console, 'Connection error:'));
 dbMongo.once('open', function() {
   console.log('Connected to DB successfully!');
-  app.listen(PORT, () =>
+  appExpress.listen(PORT, () =>
     console.log(`App is running on http://localhost:${PORT}`)
   );
 });
--- src/httpStatusCode.js
+++
@@ -1,8 +0,0 @@
-const HTTP_STATUS_CODE = {
-  badRequest: 400,
-  notFound: 404,
-  OK: 200,
-  created: 201,
-};
-
-module.exports = { HTTP_STATUS_CODE };
